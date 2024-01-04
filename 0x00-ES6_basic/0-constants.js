f8e977323186976581d4dc14d2864a3eb4a738c6
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-04 03:15:22 +0200

    0. Const or let?

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,23 @@
+/**
+ * 0. Const or let? (mandatory)
+	Modify
+i
+	function taskFirst to instantiate variables using const
+	function taskNext to instantiate variables using let
+*/
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
