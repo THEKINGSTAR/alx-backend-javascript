580ff9d494c880dc53fbbfb4f229e9742ae7ee2f
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-08 01:12:34 +0200

    test task 11 outputs

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
+  const emd_dp = {
+    [departmentName]: employees.map(emp => ({ ...emp })),
+  };
 
+  return emd_dp;
 }
+
