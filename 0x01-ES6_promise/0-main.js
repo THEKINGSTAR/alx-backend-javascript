13505dc8dd6a9ebb2bf0e6d301d469b1984984dc
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-10 09:29:44 +0200

    UPDATE README

---
+++ 0x01-ES6_promise/0-main.js
@@ -0,0 +1,11 @@
+import getResponseFromAPI from "./0-promise.js";
+
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
+
+/*
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 0-main.js 
+true
+bob@dylan:~$
+*/
