c0b68247d1caa51d59b4faf60ce75604e955dcc5
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-11 07:15:01 +0200

    1. Let's build a Teacher interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,3 +1,5 @@
+import * as StackUtils from "stack-utils";
+
 interface Student
 { 
     firstName:string;
@@ -22,9 +24,7 @@ const student2: Student = {
 
 
 
-studentsList: Array;
-
-let studentsList = [student1, student2];
+let studentsList: Student[] = [student1, student2];
 
 const table = document.createElement('table');
 
@@ -33,9 +33,18 @@ studentsList.forEach((student) => {
 const row = table.insertRow();
 const cell1 = row.insertCell(0);
 const cell2 = row.insertCell(1);
+const cell3 = row.insertCell(2);
+const cell4 = row.insertCell(3);
 
 cell1.textContent = student.firstName;
-cell2.textContent = student.location;
+cell2.textContent = student.lastName;
+// Convert age to string before assigning
+cell3.textContent = student.age.toString();
+cell4.textContent = student.location;
+
+
+
+
 });
 
 
