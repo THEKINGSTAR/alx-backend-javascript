8147d327c09e408a158c75321468aa92f9474a82
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-11 08:09:05 +0200

    1. Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,85 @@
+
+//These two attributes should only be modifiable when a Teacher is first initialized
+interface Teacher {
+    readonly firstName  : string;
+    readonly lastName : string;
+
+
+fullTimeEmployee : boolean; //this attribute should always be defined
+yearsOfExperience? : number; //this attribute is optional
+readonly location : string; //this attribute should always be defined
+
+//Add the possibility to add any attribute to the Object like 
+//contract : boolean; //without specifying the name of the attribute
+[key: string]: any;
+
+}
+
+
+
+
+
+
+
+
+//Example:
+const teacher1: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    yearsOfExperience: 30,
+    lastName: 'Wick',
+    location: 'Italy',
+    contract: false,
+  };
+  console.log(teacher1);
+
+  const teacher2: Teacher = {
+    firstName: 'Alan',
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    lastName: 'Eaa',
+    location: 'Parix',
+    contract: true,
+  };
+  console.log(teacher2);
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+// should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
+
+
+let teachersList: Teacher[] = [teacher1, teacher2, teacher3];
+const table = document.createElement('table');
+
+teachersList.forEach((teacher) => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+    const cell5 = row.insertCell(4);
+
+    cell1.textContent = teacher.firstName;
+    cell2.textContent = teacher.lastName;
+    cell3.textContent = teacher.fullTimeEmployee.toString();
+    cell4.textContent = teacher.location;
+    cell5.textContent = teacher.yearsOfExperience.toString();
+
+
+});
+
+document.body.appendChild(table);
+
