9f78233078a8f7db53b0e1e7669ec9b57915d61c
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-12 03:32:49 +0200

    2. Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,47 +1,38 @@
-
-//These two attributes should only be modifiable when a Teacher is first initialized
+// These two attributes should only be modifiable when a Teacher is first initialized
 interface Teacher {
-    readonly firstName  : string;
-    readonly lastName : string;
+    readonly firstName: string;
+    readonly lastName: string;
 
+fullTimeEmployee: boolean; // this attribute should always be defined
+yearsOfExperience? : number; // this attribute is optional
+readonly location: string; // this attribute should always be defined
 
-fullTimeEmployee : boolean; //this attribute should always be defined
-yearsOfExperience? : number; //this attribute is optional
-readonly location : string; //this attribute should always be defined
-
-//Add the possibility to add any attribute to the Object like 
-//contract : boolean; //without specifying the name of the attribute
+// Add the possibility to add any attribute to the Object like
+// contract : boolean; //without specifying the name of the attribute
 [key: string]: any;
 
 }
 
-
-
-
-
-
-
-
-//Example:
+// Example:
 const teacher1: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    yearsOfExperience: 30,
-    lastName: 'Wick',
-    location: 'Italy',
-    contract: false,
-  };
-  console.log(teacher1);
+  firstName: 'John',
+  fullTimeEmployee: false,
+  yearsOfExperience: 30,
+  lastName: 'Wick',
+  location: 'Italy',
+  contract: false,
+};
+console.log(teacher1);
 
-  const teacher2: Teacher = {
-    firstName: 'Alan',
-    fullTimeEmployee: true,
-    yearsOfExperience: 10,
-    lastName: 'Eaa',
-    location: 'Parix',
-    contract: true,
-  };
-  console.log(teacher2);
+const teacher2: Teacher = {
+  firstName: 'Alan',
+  fullTimeEmployee: true,
+  yearsOfExperience: 10,
+  lastName: 'Eaa',
+  location: 'Parix',
+  contract: true,
+};
+console.log(teacher2);
 
 const teacher3: Teacher = {
   firstName: 'John',
@@ -60,26 +51,32 @@ console.log(teacher3);
 // lastName: "Doe"
 // location: "London"
 
+// Path: js/main.ts
+interface Directors extends Teacher{
+  readonly numberOfReports: number;
+}
 
-let teachersList: Teacher[] = [teacher1, teacher2, teacher3];
+const teachersList: Teacher[] = [teacher1, teacher2, teacher3];
+
+// eslint-disable-next-line no-undef
 const table = document.createElement('table');
 
 teachersList.forEach((teacher) => {
-    const row = table.insertRow();
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    const cell4 = row.insertCell(3);
-    const cell5 = row.insertCell(4);
+  const row = table.insertRow();
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  const cell5 = row.insertCell(4);
+  const cell6 = row.insertCell(5);
 
-    cell1.textContent = teacher.firstName;
-    cell2.textContent = teacher.lastName;
-    cell3.textContent = teacher.fullTimeEmployee.toString();
-    cell4.textContent = teacher.location;
-    cell5.textContent = teacher.yearsOfExperience.toString();
-
-
+  cell1.textContent = teacher.firstName;
+  cell2.textContent = teacher.lastName;
+  cell3.textContent = teacher.fullTimeEmployee.toString();
+  cell4.textContent = teacher.location;
+  cell5.textContent = teacher.yearsOfExperience.toString();
+  cell6.textContent = teacher.numberOfReports.toString();
 });
 
+// eslint-disable-next-line no-undef
 document.body.appendChild(table);
-
--- 0x04-TypeScript/webpack.config.js
+++ 0x04-TypeScript/webpack.config.js
@@ -1,10 +1,13 @@
 
+
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: "./js/main.ts",
   devtool: "inline-source-map",
   module: {
@@ -36,4 +39,3 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   }
 };
-
