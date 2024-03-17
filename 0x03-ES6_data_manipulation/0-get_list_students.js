d3d622e7c6e9297b73a2ffe9dfced4d3626a31d8
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-17 19:07:24 +0200

    CORRECT THE OUTPUTS OF HTE FUNCTION :

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,11 @@
 export default function getListStudents() {
   const students = new Map();
 
-  students.set(1, { firstName: 'Guillaume', id: 1, location: 'San Francisco' });
-  students.set(2, { firstName: 'Guillaume', id: 1, location: 'San Francisco' });
-  students.set(3, { firstName: 'Guillaume', id: 1, location: 'San Francisco' });
+  students.set(1, { id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  students.set(2, { id: 2, firstName: 'James', location: 'Columbia' });
+  students.set(3, { id: 5, firstName: 'Serena', location: 'San Francisco' });
 
-  const studentArray = [];
-
-  students.forEach((value) => {
-    studentArray.push(value);
-  });
+  const studentArray = Array.from(students.values());
 
   return studentArray;
 }
