3e023f9e6df7bca5680abd6d9d946f5b09b85927
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-05 17:55:13 +0300

    2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,25 @@
+/*
+Append three handlers to the function:
+When the Promise resolves, return an object with the following attributes
+status : 200
+body : success
+When the Promise rejects, return an empty Error object
+For every resolution, log Got a response from the API to the console
+*/
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error());
+    }
+  });
+}
+handleResponseFromAPI(true)
+  .then((/* response */) => {
+    console.log('Got a response from the API');
+    // console.log(response);
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  });
