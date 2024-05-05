955815c67177dd39f46daff4db1a68c583c39655
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-05 18:00:23 +0300

    recorrent the return value and CORRECT THE OUTPUT OF 2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -9,7 +9,7 @@ For every resolution, log Got a response from the API to the console
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
-      resolve({ status: 200, body: 'Success' });
+      resolve({ status: 200, body: 'success' });
     } else {
       reject(new Error());
     }
