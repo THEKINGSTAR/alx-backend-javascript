29654c666691a97367956141fe5880925c916de8
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-05 18:51:27 +0300

    WORKING ON THE PROMISE

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -7,19 +7,7 @@ When the Promise rejects, return an empty Error object
 For every resolution, log Got a response from the API to the console
 */
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise.then(() => ({ status: 200, body: 'auccess' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-handleResponseFromAPI(true)
-  .then((response) => response)
-  .catch((error) => {
-    console.log('Error', error);
-  })
-  .finally(() => {
-    console.log('Got a response from the API');
-  });
